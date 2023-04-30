caf75f03ee47b98b7c081ab49e908d85f62020ab
xl33zy <baubekzhumabaev@gmail.com>
2023-04-30 18:37:17 +0600

    edited MyLinkedList

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -1,84 +1,109 @@
-import java.util.Iterator;
+import java.util.NoSuchElementException;
 
-public class MyLinkedList<T> implements Iterable<T> {
-    private Node<T> head;
-    private Node<T> tail;
+public class MyLinkedList<E> {
+    private Node<E> head;
     private int size;
 
-    private class Node<T> {
-        private T element;
-        private Node<T> next;
+    private static class Node<E> {
+        E data;
+        Node<E> next;
 
-        public Node(T element) {
-            this.element = element;
+        public Node(E data) {
+            this.data = data;
         }
     }
 
-    public void add(T element) {
-        Node<T> newNode = new Node<>(element);
-        if (size == 0) {
-            head = newNode;
-        } else {
-            tail.next = newNode;
-        }
-        tail = newNode;
+    public MyLinkedList() {
+        head = null;
+        size = 0;
+    }
+
+    public void addFirst(E element) {
+        Node<E> newNode = new Node<>(element);
+        newNode.next = head;
+        head = newNode;
         size++;
     }
 
-    public T get(int index) {
-        if (index < 0 || index >= size) {
-            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+    public E removeFirst() {
+        if (head == null) {
+            throw new NoSuchElementException();
         }
-        Node<T> current = head;
+        E removed = head.data;
+        head = head.next;
+        size--;
+        return removed;
+    }
+
+    public E getFirst() {
+        if (head == null) {
+            throw new NoSuchElementException();
+        }
+        return head.data;
+    }
+
+    public void add(int index, E element) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (index == 0) {
+            addFirst(element);
+            return;
+        }
+        Node<E> newNode = new Node<>(element);
+        Node<E> current = head;
+        for (int i = 0; i < index - 1; i++) {
+            current = current.next;
+        }
+        newNode.next = current.next;
+        current.next = newNode;
+        size++;
+    }
+
+    public E remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (index == 0) {
+            return removeFirst();
+        }
+        Node<E> current = head;
+        for (int i = 0; i < index - 1; i++) {
+            current = current.next;
+        }
+        E removed = current.next.data;
+        current.next = current.next.next;
+        size--;
+        return removed;
+    }
+
+    public E get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node<E> current = head;
         for (int i = 0; i < index; i++) {
             current = current.next;
         }
-        return current.element;
-    }
-
-    public T remove(int index) {
-        if (index < 0 || index >= size) {
-            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
-        }
-        T removedElement;
-        if (index == 0) {
-            removedElement = head.element;
-            head = head.next;
-            if (size == 1) {
-                tail = null;
-            }
-        } else {
-            Node<T> previous = head;
-            for (int i = 0; i < index - 1; i++) {
-                previous = previous.next;
-            }
-            removedElement = previous.next.element;
-            previous.next = previous.next.next;
-            if (index == size - 1) {
-                tail = previous;
-            }
-        }
-        size--;
-        return removedElement;
+        return current.data;
     }
 
     public int size() {
         return size;
     }
 
-    public Iterator<T> iterator() {
-        return new Iterator<T>() {
-            private Node<T> current = head;
+    public boolean isEmpty() {
+        return size == 0;
+    }
 
-            public boolean hasNext() {
-                return current != null;
+    public boolean contains(E element) {
+        Node<E> current = head;
+        while (current != null) {
+            if (current.data.equals(element)) {
+                return true;
             }
-
-            public T next() {
-                T element = current.element;
-                current = current.next;
-                return element;
-            }
-        };
+            current = current.next;
+        }
+        return false;
     }
 }
--- src/MyLinkedListStack.java
+++ src/MyLinkedListStack.java
@@ -1,20 +1,11 @@
 public class MyLinkedListStack<T> {
-    private Node<T> head;
-    private int size;
     private MyLinkedList<T> list;
-
-    private class Node<T> {
-        private T element;
-        private Node<T> next;
-
-        public Node(T element) {
-            this.element = element;
-        }
-    }
 
     public MyLinkedListStack() {
         list = new MyLinkedList<>();
     }
 
-
+    public void push(T element) {
+        list.addFirst(element);
+    }
 }
