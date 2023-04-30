241aa80cd5fbb09433d101c18c83b48fc804a5a1
xl33zy <baubekzhumabaev@gmail.com>
2023-04-30 19:07:43 +0600

    edited Main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("            --------Testing Linked List Stack--------");
+        testLinkedListStack();
+        System.out.println("            --------Testing Linked List Queue--------");
+        testLinkedListQueue();
+    }
+
+    private static void testLinkedListStack() {
+
+    }
+
+    private static void testLinkedListQueue() {
+
     }
 }
 
