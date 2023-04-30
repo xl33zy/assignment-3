18ccd060c7e99ec2398d15873f3312ffc0020650
xl33zy <baubekzhumabaev@gmail.com>
2023-04-30 18:19:20 +0600

    added Node class to MyLinkedListStack

--- src/MyLinkedListStack.java
+++ src/MyLinkedListStack.java
@@ -1,6 +1,15 @@
 public class MyLinkedListStack<T> {
     private MyLinkedList<T> list;
 
+    private class Node<T> {
+        private T element;
+        private Node<T> next;
+
+        public Node(T element) {
+            this.element = element;
+        }
+    }
+
     public MyLinkedListStack() {
         list = new MyLinkedList<>();
     }
